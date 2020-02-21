0c16e739f8880fe970e7c738198e0a7ef67f9a09
Lorenzo T. Tuppince <lorenzot.tuppince@Macs-MacBook-Pro.local>
2020-02-20 20:42:37 -0500

    updated models

--- models/message.js
+++ models/message.js
@@ -1,7 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
     var Message = sequelize.define("Message", {
-      name: DataTypes.STRING
+      author_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      recipient_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      }    
     });
+
+    Message.associate = function(models) {
+      // We're saying that a Post should belong to an Author
+      // A Post can't be created without an Author due to the foreign key constraint
+      Message.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        },
+        as:"AuthorId"
+      });
+    };
+
+    Message.associate = function(models) {
+      // We're saying that a Post should belong to an Author
+      // A Post can't be created without an Author due to the foreign key constraint
+      Message.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        },
+        as:"RecipientId"
+      });
+    };
+  
     
     return Message;
 };
