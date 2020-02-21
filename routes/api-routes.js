66ef29a550738941254633318b90f3ef1e5ea214
rpharr <rpharr@richmond.edu>
2020-02-21 14:15:32 -0500

    save

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,10 +14,12 @@ module.exports = function(app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", function(req, res) {
+    console.log(req.body.username, req.body.email, req.body.password);
     db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      })
       .then(function() {
         res.redirect(307, "/api/login");
       })
@@ -41,6 +43,7 @@ module.exports = function(app) {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
+        // username: req.user.username,
         email: req.user.email,
         id: req.user.id
       });
