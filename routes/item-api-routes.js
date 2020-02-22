ec8cf66a7fcc260366bf220a156fe3422269bed9
haloscot <hannahlorrainescott@gmail.com>
2020-02-22 11:29:47 -0500

    making api routes.  need checked i have no idea if these make sense

---
+++ routes/item-api-routes.js
@@ -0,0 +1,63 @@
+// Requiring our models and passport as we've configured it
+var db = require("../models");
+var passport = require("../config/passport");
+module.exports = function(app) {
+  // GET route for all items
+  app.get("/api/items", function(req, res) {
+    if (!req.user) {
+      res.json({});
+    } else {
+      db.Item.findAll({
+        where: query,
+        include: [db.User]
+      }).then(function(dbItem) {
+        res.json(dbItem);
+      });
+    }
+  });
+
+  // GET route for single item
+  app.get("/api/items/:id", function(req, res) {
+    db.Item.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbItem) {
+      console.log(dbItem);
+      res.json(dbItem);
+    });
+  });
+
+  // POST route for saving new item
+  app.post("/api/items", function(req, res) {
+    db.Item.create(req.body).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+
+  // DELETE route for deleting item
+  app.delete("/api/items/:id", function(req, res) {
+    db.Item.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  // PUT route for updating items
+  app.put("/api/items", function(req, res) {
+    db.Item.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+};
