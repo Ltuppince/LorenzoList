7e21d00be12bdca9c471215cc31959a082c119ec
rpharr <rpharr@richmond.edu>
2020-02-23 22:42:03 -0500

    save

--- routes/item-api-routes.js
+++ routes/item-api-routes.js
@@ -8,8 +8,17 @@ module.exports = function(app) {
       res.json({});
     } else {
       db.Item.findAll({
-        where: query,
-        include: [db.User]
+        //where: query,
+        // include: [{
+        //     model: db.User,
+        //     as: "User",
+        //     through: {
+        //       attributes: ['username'],
+        //       //where: {completed: true}
+        //     }
+        //   }]
+        // include: [{ model: db.User, as: "User" }]
+        include: [db.User.username]
       }).then(function(dbItem) {
         res.json(dbItem);
       });
@@ -50,8 +59,7 @@ module.exports = function(app) {
   // PUT route for updating items
   app.put("/api/items", function(req, res) {
     db.Item.update(
-      req.body,
-      {
+      req.body, {
         where: {
           id: req.body.id
         }
